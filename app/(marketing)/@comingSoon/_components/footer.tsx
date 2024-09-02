60e4f8bec206c91a87af3f7e46eab1bc0d8c52ff
Collin Schaafsma <schaafsma@gmail.com>
2024-09-02 17:34:09 -0600

    use appName in coming soon footer

--- app/(marketing)/@comingSoon/_components/footer.tsx
+++ app/(marketing)/@comingSoon/_components/footer.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import { appName } from "@/lib/constants"
 
 export default function Footer() {
   return (
     <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
       <p className="text-xs text-gray-500 dark:text-gray-400">
-        © 2024 Schaafsma LLC. All rights reserved.
+        © 2024 {appName}. All rights reserved.
       </p>
       <nav className="hidden gap-4 sm:ml-auto sm:gap-6">
         <Link className="text-xs underline-offset-4 hover:underline" href="#">
