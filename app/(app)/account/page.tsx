5d89b4041ec2e9cd512d542e9adcaa96aedd518a
Collin Schaafsma <schaafsma@gmail.com>
2024-10-16 21:24:59 -0600

    await search params

--- app/(app)/account/page.tsx
+++ app/(app)/account/page.tsx
@@ -14,16 +14,17 @@ export const metadata = {
   title: "Account",
 }
 
-export default function AccountPage({
+export default async function AccountPage({
   searchParams,
 }: {
   searchParams: { page: string }
 }) {
-  const page: number = searchParams.page ? parseInt(searchParams.page) : 1
+  const { page } = await searchParams
+  const pageNumber: number = page ? parseInt(page) : 1
   // Preload data for the account page
   currentUser.paymentMethods({ limit: 1 })
   currentUser.subscriptions({ limit: 1 })
-  currentUser.invoices({ page })
+  currentUser.invoices({ page: pageNumber })
 
   return (
     <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
@@ -41,7 +42,7 @@ export default function AccountPage({
           <PaymentMethodCard />
           <SubscriptionPlanCard />
           <div className="flex flex-col gap-4 md:col-span-2">
-            <InvoiceCard page={page} />
+            <InvoiceCard page={pageNumber} />
           </div>
         </div>
       </main>
